c7c7358b19ef9aad7dc5523bc0bf6e4f82639c22
raihannishat <raihannishat.swe@gmail.com>
2020-10-24 20:06:46 +0600

    Class 1

---
+++ Course_1/Chap_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chap_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chap_1");
+        }
+    }
+}
---
+++ Course_1/Chap_2/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Chap_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int a;
+            int b;
+            int sum;
+
+            a = 50;
+            b = 60;
+
+            sum = a + b;
+
+            Console.WriteLine("Sum is {0}", sum);
+            */
+
+            /*
+            int a, b, sum;
+
+            a = 50;
+            b = 60;
+
+            sum = a + b;
+
+            Console.WriteLine("Sum is {0}", sum);
+            */
+
+            /*
+            int a = 50, b = 60, sum;
+
+            sum = a + b;
+
+            Console.WriteLine("Sum is {0}", sum);
+            */
+
+            /*
+            int x, y;
+
+            x = 1;
+            y = x;
+            x = 2;
+
+            Console.WriteLine("{0}", y);
+            */
+
+            /*
+            int a = 50, b = 60, sum;
+
+            sum = a + b;
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, sum);
+            */
+
+            /*
+            int a = 50.45, b = 60, sum;
+
+            sum = a + b;
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, sum);
+            */
+
+            /*
+            int n;
+            double x;
+
+            x = 10.5;
+            n = (int)x;
+
+            Console.WriteLine("Value of n is {0}", n);
+            Console.WriteLine("Value of x is {0}", x);
+            */
+
+            /*
+            int a;
+
+            a = 1000;
+            Console.WriteLine("Value of a is {0}", a);
+
+            a = -21000;
+            Console.WriteLine("Value of a is {0}", a);
+
+            a = 10000000;
+            Console.WriteLine("Value of a is {0}", a);
+
+            a = -10000000;
+            Console.WriteLine("Value of a is {0}", a);
+
+            a = 100020004000503;
+            Console.WriteLine("Value of a is {0}", a);
+
+            a = -4325987632;
+            Console.WriteLine("Value of a is {0}", a);
+            */
+
+            /*
+            double a, b, sum;
+
+            a = 9.5;
+            b = 8.743;
+
+            sum = a + b;
+
+            Console.WriteLine("Sum is {0}", sum);
+
+            Console.WriteLine("Sum is {0:0.##}", sum);
+            */
+
+            /*
+            int a, b, sum;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            sum = a + b;
+
+            Console.WriteLine("Sum is {0}", sum);
+            */
+        }
+    }
+}
