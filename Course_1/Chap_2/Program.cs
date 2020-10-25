343c46ce2e807f1e7fdd72099587711c6ddfa8be
raihannishat <raihannishat.swe@gmail.com>
2020-10-25 20:08:41 +0600

    Class_3

--- Course_1/Chap_2/Program.cs
+++ Course_1/Chap_2/Program.cs
@@ -105,9 +105,9 @@ namespace Chap_2
 
             sum = a + b;
 
-            Console.WriteLine("Sum is {0}", sum);
+            Console.WriteLine("1st: Sum is {0}", sum);
 
-            Console.WriteLine("Sum is {0:0.##}", sum);
+            Console.WriteLine("2nd: Sum is {0:0.##}", sum);
             */
 
             /*
@@ -120,6 +120,69 @@ namespace Chap_2
 
             Console.WriteLine("Sum is {0}", sum);
             */
+
+            /*
+            char ch;
+
+            Console.Write("Enter the first letter of your name: ");
+
+            ch = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("The first letter of your name: {0}", ch);
+            */
+
+            /*
+            int num1, num2;
+
+            Console.Write("Please enter a number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter another number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+            */
+
+            /*
+            int num1, num2, value;
+            char sign;
+
+            Console.Write("Please enter a number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter another number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            value = num1 + num2;
+            sign = '+';
+            Console.WriteLine("{0} {1} {2} = {3}", num1, sign, num2, value);
+
+            value = num1 - num2;
+            sign = '-';
+            Console.WriteLine("{0} {1} {2} = {3}", num1, sign, num2, value);
+
+            value = num1 * num2;
+            sign = '*';
+            Console.WriteLine("{0} {1} {2} = {3}", num1, sign, num2, value);
+
+            value = num1 / num2;
+            sign = '/';
+            Console.WriteLine("{0} {1} {2} = {3}", num1, sign, num2, value);
+            */
+
+            // test program - comment 1
+
+            Console.Write("Hello ");
+
+            /* We have printed Hello,
+             * now we shall print World
+             * Note that this is a multi-line comment
+             */
+
+            Console.WriteLine("World"); // printed world
         }
     }
 }
