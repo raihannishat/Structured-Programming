2b62c39e8c3fd4127514c1b18323a6781b0a4bfe
raihannishat <raihannishat.swe@gmail.com>
2020-10-30 18:38:56 +0600

    Class_3

---
+++ Course_1/Chap_3/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Chap_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int n;
+
+            n = 10;
+
+            if (n >= 0)
+            {
+                Console.WriteLine("The number is positive");
+            }
+            else
+            {
+                Console.WriteLine("The number is negative");
+            }
+            */
+
+            /*
+            int n;
+
+            n = 0;
+
+            if (n > 0)
+            {
+                Console.WriteLine("The number is positive");
+            }
+            else
+            {
+                Console.WriteLine("The number is negative");
+            }
+            */
+
+            /*
+            int n = 10;
+
+            if (n < 0)
+            {
+                Console.WriteLine("The number is negative");
+            }
+            else if (n > 0)
+            {
+                Console.WriteLine("The number is positive");
+            }
+            else if (n == 0)
+            {
+                Console.WriteLine("The number is zero!");
+            }
+            */
+
+            /*
+            int n = 0;
+
+            if (n < 0)
+            {
+                Console.WriteLine("The number is negative");
+            }
+            else if (n > 0)
+            {
+                Console.WriteLine("The number is positive");
+            }
+            else
+            {
+                Console.WriteLine("The number is zero!");
+            }
+            */
+
+            /*
+            int number = 12;
+
+            if (number > 10)
+            {
+                Console.WriteLine("The number is greater than ten.");
+            }
+            */
+        }
+    }
+}
