2ef1178083e119aa64c6636982cdfb022abd87c4
raihannishat <raihannishat.swe@gmail.com>
2020-10-31 17:17:34 +0600

    cpbook 1st part for C#

---
+++ cpbook 1st part/Chap_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Chap_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Code: 1.1
+            Console.WriteLine("Hello World!");
+            #endregion
+        }
+    }
+}
