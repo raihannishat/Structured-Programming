045904da2249167d21b57ba36e8126c2af482a8a
raihannishat <raihannishat.swe@gmail.com>
2021-01-06 16:47:32 +0600

    Chap_07_More_Pointer

---
+++ Computer.Programming.Second.Part/Chap_07_More_Pointer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chap_07_More_Pointer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
