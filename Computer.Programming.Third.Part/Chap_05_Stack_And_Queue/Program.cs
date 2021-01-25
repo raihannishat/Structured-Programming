7086bb4f16e14ffa266a73af47c574beb1c17cc2
raihannishat <raihannishat.swe@gmail.com>
2021-01-25 08:32:25 +0600

    Code

---
+++ Computer.Programming.Third.Part/Chap_05_Stack_And_Queue/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Chap_05_Stack_And_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack my_stack = new Stack();
+
+            my_stack.Push(1);
+            my_stack.Push(2);
+            my_stack.Push(3);
+
+            Console.WriteLine(my_stack.Pop());
+            Console.WriteLine(my_stack.Pop());
+            Console.WriteLine(my_stack.Pop());
+            Console.WriteLine(my_stack.Pop());
+
+            Balanced balanced = new Balanced();
+
+            string input = Console.ReadLine();
+
+            if (Convert.ToBoolean(balanced.is_balanced(input.ToCharArray())))
+            {
+                Console.WriteLine($"{input} is balanced");
+            }
+            else
+            {
+                Console.WriteLine($"{input} is not balanced");
+            }
+
+            Bracket bracket = new Bracket();
+
+            var str = "[({})]";
+
+            if (bracket.IsBalanced(str))
+            {
+                Console.WriteLine($"{str} is balanced");
+            }
+            else
+            {
+                Console.WriteLine($"{str} is not balanced");
+            }
+        }
+    }
+}
---
+++ Computer.Programming.Third.Part/Chap_05_Stack_And_Queue/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chap_05_Stack_And_Queue
+{
+    public class Stack
+    {
+        private const int STACK_MAX = 100;
+        private int top;
+        private int[] data;
+
+        public Stack()
+        {
+            top = 0;
+            data = new int[STACK_MAX];
+        }
+
+        public void Push(int item)
+        {
+            if (top < STACK_MAX)
+            {
+                data[top] = item;
+                top++;
+            }
+            else
+            {
+                Console.WriteLine("Stack is full!");
+            }
+        }
+
+        public int Pop()
+        {
+            if (top == 0)
+            {
+                Console.WriteLine("Stack is empty!");
+                return -1;
+            }
+            else
+            {
+                top--;
+                return data[top];
+            }
+        }
+    }
+}
